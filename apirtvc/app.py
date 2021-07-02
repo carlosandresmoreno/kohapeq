5843a19c76c3a3a5e428ccd60fedee1034487b86
5carlosmoreno2021 <5carlosmoreno2021@gmail.com>
2021-07-02 14:43:57 -0500

    first commit

---
+++ apirtvc/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from operationBD import koha
+
+
+
+app = Flask(__name__)
+
+
+@app.route ('/')
+def index():
+    return '<h1>hola mundo</h1>'
+
+@app.route ('/get', methods = ['POST'])
+def get():            
+    kohas =koha()
+    res =kohas.getJson(request.json)
+    return jsonify(res)
+
+
+@app.route ('/insert', methods = ['POST'])
+def insert():            
+    kohas =koha()
+    res = kohas.insertJson(request.json )
+    return jsonify(res)
+
+
+@app.route ('/update', methods = ['POST'])
+def update():            
+    kohas =koha()
+    res =kohas.updateJson(request.json)
+    return jsonify(res)
+     
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
